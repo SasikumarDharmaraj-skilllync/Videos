97266fd5d4ce17bab5d471be0143610898b90aea
SasikumarDharmaraj-skilllyn <sasikumar.dharmaraj@skill-lync.com>
2022-06-30 22:45:52 +0530

    video thumbnail removed

--- src/components/thumbnail.js
+++ src/components/thumbnail.js
@@ -8,14 +8,14 @@ const Thumbnail = () => {
     return (
         <>
             <Link href="/videoPlay">
-                <div onClick={sasi}>
-                    <VideoThumbnail
-                        videoUrl="https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4"
-                        thumbnailHandler={(thumbnail) => console.log(thumbnail)}
-                        width={120}
-                        height={80}
-                    />
-                </div>
+                {/* <div onClick={sasi}> */}
+                <VideoThumbnail
+                    videoUrl="https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4"
+                    thumbnailHandler={(thumbnail) => console.log(thumbnail)}
+                    width={120}
+                    height={80}
+                />
+                {/* </div> */}
 
             </Link>
         </>
--- src/components/verticalCard.js
+++ src/components/verticalCard.js
@@ -8,15 +8,19 @@ const VerticalCard = () => {
             <div class="card text-dark mb-3">
                 <div class="card-body">
                     <div className="row">
-                        <div className="col-6">
+                        <div className="col-5">
                             <Link href="/videoPlay">
-                                <img src='https://www.w3schools.com/images/w3schools_green.jpg' alt='sasa' />
+                                <img src='https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/weather/autumn/autumn-leaves-against-a-blue-sky-photo-andrew-small.jpg' width="400px" height="250px" alt='sasa' style={{ border: "2px solid black" }} />
                             </Link>
                         </div>
-                        <div className="col-6">
-                            <p>Title: Full Stack Development</p>
-                            <p>Description: It was a computer science oriented Programme</p>
-                            <p>Date: 30-06-2022 </p>
+                        <div className="col-7">
+                            <div className="card border-success mb-3">
+                                <div className="card-header bg-transparent border-success">Title: Full Stack Development</div>
+                                <div className="card-body text-success">
+                                    <h5 className="card-title">Description: It was a computer science oriented Programme.</h5>
+                                </div>
+                                <div class="card-footer bg-transparent border-success">Date: 30-06-2022</div>
+                            </div>
                         </div>
                     </div>
                 </div>
